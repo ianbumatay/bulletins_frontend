701480e03b353018b19c6f4ba43fd70c83feff3e
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-12-07 22:08:57 -0500

    build fetch for bulletin/index and created bulletin js objects

---
+++ src/bulletins.js
@@ -0,0 +1,7 @@
+class Bulletin{
+    constructor(id, title, content){
+        this.id = id 
+        this.title = title 
+        this.content = content
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("connecting...")
+console.log("connecting...") 
+
+// test that we can get data from the backend
+const BACKEND_URL = "http://localhost:3000"; 
+
+fetch(`${BACKEND_URL}/bulletins`)
+  .then(response => response.json())
+  .then(parsedBulletins => {
+
+    for(const bulletin of parsedBulletins) { 
+        console.log("rails obj", bulletin) 
+        let b = new Bulletin(bulletin.id, bulletin.title, bulletin.content)
+        console.log("js obj", b)
+    }
+  })
