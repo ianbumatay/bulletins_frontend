e36c1ea2c086ba5b7494f59f0640be977edfe54a
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-12-09 17:51:11 -0500

    created submitform function

--- src/bulletins.js
+++ src/bulletins.js
@@ -1,17 +1,17 @@
-class Bulletin{
-    constructor(id, title, content){
-        this.id = id 
-        this.title = title 
-        this.content = content
-    } 
-    renderBulletin(){
-        const divTag = document.querySelector("#li-container") 
-        divTag.innerHTML += 
-        `
+// class Bulletin{
+//     constructor(id, title, content){
+//         this.id = id 
+//         this.title = title 
+//         this.content = content
+//     } 
+//     renderBulletin(){
+//         const divTag = document.querySelector("#bullets-container") 
+//         divTag.innerHTML += 
+//         `
       
-          <h2> ${this.title} </h2>
-          <p> ${this.content} </p>
+//           <h2> ${this.title} </h2>
+//           <p> ${this.content} </p>
 
-        `
-    }
-}
+//         `
+//     }
+// }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,54 @@
 console.log("connecting...") 
 
 // test that we can get data from the backend
-const BACKEND_URL = "http://localhost:3000/"; 
+// const BACKEND_URL = "http://localhost:3000/"; 
 
-fetch(`${BACKEND_URL}/bulletins`)
-  .then(response => response.json())
-  .then(parsedBulletins => {
+// fetch(`${BACKEND_URL}/bulletins`)
+//   .then(response => response.json())
+//   .then(parsedBulletins => {
 
-    for(const bulletin of parsedBulletins) { 
-        console.log("rails obj", bulletin) 
-        let b = new Bulletin(bulletin.id, bulletin.title, bulletin.content)
-        console.log("js obj", b) 
-        b.renderBulletin()
-    }
-  })
+//     for(const bulletin of parsedBulletins) { 
+//         console.log("rails obj", bulletin) 
+//         let b = new Bulletin(bulletin.id, bulletin.title, bulletin.content)
+//         console.log("js obj", b) 
+//         b.renderBulletin()
+//     }
+//   }) 
+
+ const BACKEND_URL = "http://localhost:3000/";  
+
+ const bulletinForm = document.querySelector("#bulletin-form") 
+ //console.log(bulletinForm) 
+
+ bulletinForm.addEventListener("submit", submitForm) 
+     //console.log("clicked")
+ function submitForm(e){
+   e.preventDefault() 
+
+   const title = document.querySelector("#title").value
+   const content = document.querySelector("#content").value
+   //console.log(title, content)  
+
+   
+   //fetch(`${BACKEND_URL}/bulletins`, option)
+
+ }
+
+ //submitForm()
+
+// function fetchBulletin(){
+//   fetch(`${BACKEND_URL}/bulletins`)
+//   .then(response => response.json())
+//   .then(bulletinObj => renderBulletin(bulletinObj))
+
+// }  
+
+// function renderBulletin(){ 
+
+//   const div = document.querySelector("#bullets-container")
+  
+// }
+
+
+
+
