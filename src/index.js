d1f0c78e05d12e7eaefac60ced4bf585aa0af2b5
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-12-10 12:23:52 -0500

    added data and attributes in fetchBUlleetin

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 console.log("connecting...") 
 
-// test that we can get data from the backend
-// const BACKEND_URL = "http://localhost:3000/"; 
-
-// fetch(`${BACKEND_URL}/bulletins`)
-//   .then(response => response.json())
-//   .then(parsedBulletins => {
-
-//     for(const bulletin of parsedBulletins) { 
-//         console.log("rails obj", bulletin) 
-//         let b = new Bulletin(bulletin.id, bulletin.title, bulletin.content)
-//         console.log("js obj", b) 
-//         b.renderBulletin()
-//     }
-//   }) 
 
  const BACKEND_URL = "http://localhost:3000/";  
 
@@ -46,32 +32,51 @@ console.log("connecting...")
    
  } 
 
- function renderBulletin(bulletin){
-   //console.log(bulletin)
-   const div = document.querySelector("#bullets-container")
-   const htag = document.createElement("h2") 
-   const ptag = document.createElement("p") 
-   htag.innerText = bulletin.attributes.title
-   ptag.innerText = bulletin.attributes.content
-   div.appendChild(htag) 
-   div.appendChild(ptag)
- }
+//  function renderBulletin(bulletin){
+//    //console.log(bulletin)
+//    const div = document.querySelector("#bullets-container")
+//    const htag = document.createElement("h2") 
+//    const ptag = document.createElement("p") 
+//    htag.innerText = bulletin.attributes.title
+//    ptag.innerText = bulletin.attributes.content
+//    div.appendChild(htag) 
+//    div.appendChild(ptag)
+//  }
 
- //submitForm()
+//  function renderBUlletin(bulletin){ 
+//   const div = document.querySelector("#bullets-container") 
+//   div.innetHTML += 
+//   `    
+//    <h2> ${this.title} </h2>
+//    <p> ${this.content} </p>
+  
+//   `
+//  } 
+
 
 // function fetchBulletin(){
 //   fetch(`${BACKEND_URL}/bulletins`)
-//   .then(response => response.json())
-//   .then(bulletinObj => renderBulletin(bulletinObj))
-
-// }  
-
-// function renderBulletin(){ 
-
-//   const div = document.querySelector("#bullets-container")
+//    .then(response => response.json())
+//    .then(console.log)
+//    //.then(Obj => Obj.data.forEach(renderBulletin))
   
-// }
+// } 
+
+// fetchBulletin() 
 
 
+function fetchBulletin(){
+fetch(`${BACKEND_URL}/bulletins`)
+  .then(response => response.json())
+  .then(parsedBulletins => {
 
+    for(const bulletin of parsedBulletins) { 
+        console.log("rails obj", bulletin) 
+        let b = new Bulletin( bulletin.data.attributes.title, bulletin.data.attributes.content)
+        console.log("js obj", b) 
+        b.renderBulletin()
+    }
+  }) 
+} 
 
+fetchBulletin()
