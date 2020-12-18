6b4f8e595526e931cd1bbb56c85d5556f8d2d353
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-12-18 16:04:52 -0500

    clean up code in index.js file

--- src/comments.js
+++ src/comments.js
@@ -1,50 +1,45 @@
 class Comment {
-    constructor(comment){
-        this.id = comment.id 
-        this.bulletin_id = comment.attributes.bulletin_id
-        this.content = comment.attributes.content
-    }   
+  constructor(comment){
+    this.id = comment.id 
+    this.bulletin_id = comment.attributes.bulletin_id
+    this.content = comment.attributes.content
+  }   
 
-      static submitComment(e) { 
-        e.preventDefault()
+static submitComment(e) { 
+  e.preventDefault()
 
-      const userInput = e.target.children[0].value
-      const commentList = e.target.nextElementSibling
-      const bulletinId = e.target.parentElement.dataset.id 
-     
-       const createLi = document.createElement("li")
-       createLi.dataset.id = bulletinId
-       createLi.innerText = userInput
-       console.log(createLi)
-       commentList.appendChild(createLi)  
-    
-        const options = {
-          method: "POST",
-          headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({comment: {content: userInput, bulletin_id: bulletinId}})
-        } 
+const userInput = e.target.children[0].value
+const commentList = e.target.nextElementSibling
+const bulletinId = e.target.parentElement.dataset.id 
+
+const createLi = document.createElement("li")
+createLi.dataset.id = bulletinId
+createLi.innerText = userInput
+console.log(createLi)
+commentList.appendChild(createLi)  
+
+const options = {
+  method: "POST",
+  headers: {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({comment: {content: userInput, bulletin_id: bulletinId}})
+} 
+
+  fetch(`${BACKEND_URL}/comments`, options)
+    .then(response => response.json())
+    .then(comment => {
+      
+    console.log("rails" , comment)
+    let c = new Comment(comment.data)
+    console.log(c)
        
-        fetch(`${BACKEND_URL}/comments`, options)
-        .then(response => response.json())
-        .then(comment => {
-          
-          console.log("rails" , comment)
-          let c = new Comment(comment.data)
-          console.log(c)
-          
-          
-        })  
-        e.target.reset()
-      }   
+    })  
+    e.target.reset()
+  }   
 
     
-    
-    
-    
-   
 } //
 
 
