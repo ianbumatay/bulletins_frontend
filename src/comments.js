26a0c5d924700fb5b112866f0027dde056728b81
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-01-12 09:31:23 -0500

    class Comment code refactor and clean up done

--- src/comments.js
+++ src/comments.js
@@ -11,16 +11,9 @@ class Comment {
 static submitComment(e) { 
   e.preventDefault()
 
-  const userInput = e.target.children[0].value //form input 
-
-  const commentList = e.target.nextElementSibling // <ul>
-  //this.commentList = commentList
-  console.log(commentList)
-  //const commentList = document.querySelector("#comment-ul") 
-
-  const bulletinId = e.target.parentElement.dataset.id  //div. parentElement
-  console.log(e.target.parentElement) 
-
+  const userInput = e.target.children[0].value 
+  const commentList = e.target.nextElementSibling 
+  const bulletinId = e.target.parentElement.dataset.id  
 
     const options = {
       method: "POST",
@@ -35,55 +28,26 @@ static submitComment(e) {
       .then(response => response.json())
       .then(comment => {
         
-       // console.log("submit", comment)
       let c = new Comment(comment.data.attributes) 
         c.renderComment(commentList)
-        //console.log(c)
      })  
-   // e.target.reset()
+     e.target.reset()
   }    
 
 
 
   
   renderComment(commentList){  
-   
-    //const commentList = document.querySelector("#comment-ul") 
-    console.log(commentList)
 
     const createLi = document.createElement("li")
     createLi.dataset.id = this.bulletin_id
     createLi.innerText = this.content
-    console.log(this.content) 
-    console.log(this.bulletin_id)
-
-    console.log(this)
-
+  
      commentList.append(createLi)  
  }  
 
 
-
-
-  // static fetchComment(commentList){
-  //   fetch(`${BACKEND_URL}/comments`)
-  //   .then(response => response.json())
-  //   .then(commentObj => { 
-
-  //     console.log(commentObj)
-    
-  //     for(let comment of commentObj){
-  //        console.log("rails", comment)
-  //       let c = new Comment(comment.data) 
-  //       //console.log("js", c)
-  //       c.renderComment(commentList)
-  //     }
-  //  })
-  // }  
-
-
-
-}//
+}
 
 
 
